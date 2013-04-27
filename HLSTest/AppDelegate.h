b34105f369903754958573996a4501d13c6a922f
Ceaser Larry <clarry@divergentlogic.com>
2013-04-27 01:32:07 -0600

    Initial Commit

---
+++ HLSTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HLSTest
+//
+//  Created by Ceaser Larry on 4/27/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HLSTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HLSTest
+//
+//  Created by Ceaser Larry on 4/27/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
