9e0c31d11733621b08b18e1ce71d46ef280245df
Ceaser Larry <clarry@divergentlogic.com>
2013-04-28 01:48:12 -0600

    Basic example of HLS for my iPhone 4S

--- HLSTest/ViewController.h
+++ HLSTest/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@class MPMoviePlayerController;
 
+@interface ViewController : UIViewController
+@property (strong, nonatomic) MPMoviePlayerController* moviePlayerController;
 @end
--- HLSTest/ViewController.m
+++ HLSTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.moviePlayerController = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:@"http://192.168.1.41:6544/Content/GetFile?StorageGroup=Streaming&FileName=1245_20130425220000.mpg.640x368_500kV_96kA.m3u8"]];
+
+    self.moviePlayerController.movieSourceType = MPMovieSourceTypeStreaming;
+    self.moviePlayerController.fullscreen = NO;
+    self.moviePlayerController.shouldAutoplay = YES;
+    [self.moviePlayerController prepareToPlay];
+    
+    [[self.moviePlayerController view] setFrame:[self.view bounds]];
+    [self.view addSubview:[self.moviePlayerController view]];
+    
+    [self.moviePlayerController play];
 }
 
 - (void)didReceiveMemoryWarning
